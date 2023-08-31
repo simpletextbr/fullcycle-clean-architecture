dfd58e33275f6684adf68aae47f9948722076d92
Wesley Praca <simpletextbr@gmail.com>
2023-08-31 13:29:13 -0300

    Criando classe abstrata e configurando yup

---
+++ src/domain/customer/validator/customer.yup.validator.ts
@@ -0,0 +1,36 @@
+import * as yup from "yup";
+import IValidator from "../../@shared/validator/validator.interface";
+import Customer from "../entity/customer";
+
+export default class CustomerValidator implements IValidator<Customer> {
+  validate(entity: Customer): void {
+    try {
+      const schema = yup.object().shape({
+        id: yup.string().required("ID is required"),
+        name: yup.string().required("Name is required"),
+        rewardPoints: yup
+          .number()
+          .min(0, "Reward points must be greater than or equal to 0"),
+      });
+
+      schema.validateSync(
+        {
+          id: entity.id,
+          name: entity.name,
+          rewardPoints: entity.rewardPoints,
+        },
+        {
+          abortEarly: false,
+        }
+      );
+    } catch (err) {
+      const e = err as yup.ValidationError;
+      e.errors.forEach((error) => {
+        entity.notification.addError({
+          message: error,
+          context: "customer",
+        });
+      });
+    }
+  }
+}
